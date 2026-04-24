0cbe99fb00f995e4a02c7572ae775c53da3ca097
Nico Ramminger <nico.ramminger1712@gmail.com>
2026-04-24 14:56:59 +0200

    feat: initial setup

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+import pandas as pd
+import numpy as np
+from statsmodels.tsa.arima.model import ARIMA
+from pmdarima import auto_arima
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+if __name__ == "__main__":
+    print("Hello World")
